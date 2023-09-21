ae518e1eced28b478bc15aa69a5b5e8c0f88aab6
SpookyLittleGhost <felix.evers.web@gmail.com>
2023-09-21 15:50:29 +0200

    Added Peripheral functionality

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-# This is a sample Python script.
+import lib
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+import board
+import neopixel
+
+import busio
+from adafruit_ssd1306 import SSD1306_I2C
+
+import pwmio
+
+import digitalio
+import rotaryio
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def rotary_encoder_do_something():
+    while True:
+        position = encoder.position
+        if last_position is None or position != last_position:
+            print(f"Rotary: {position}")
+        last_position = position
+
+        if switch_state != switch.value:
+            switch_state = switch.value
+            print('Switch is ' + ('ON' if switch.value else 'OFF'))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def display_do_something(str):
+    display_text(str, 0)
+    display_text_black(str, 1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def buzzer_do_something():
+    buzzer.frequency = TONE_ON
+    time.sleep(Buffer)
+    buzzer.frequency = TONE_OFF
+
+
+def LED_do_something():
+    pixels[0] = LED_ON
+    time.sleep(Buffer)
+    pixels[0] = LED_OFF
+
+
+# The main method
+
+# Parameters
+LED_OFF = (0, 0, 0)
+LED_ON = (0, 20, 30)
+Buffer = 0.1
+TONE_OFF = 30000
+TONE_ON = 600
+
+#RGB LED Setup
+pixels = neopixel.NeoPixel(board.GP22, 1)
+
+# Display Setup
+i2c0 = busio.I2C(scl=board.GP17, sda=board.GP16)
+display = SSD1306_I2C(128, 64, i2c0)
+
+
+def display_text(str, line):
+    display.text(str, 0, (line % 8) * 8, 1, font_name="/lib/font5x8.bin")
+
+
+def display_text_black(str, line):
+    display.text(str, 0, (line % 8) * 8, 0, font_name="/lib/font5x8.bin")
+
+
+# Buzzer Setup
+buzzer = pwmio.PWMOut(board.GP27, variable_frequency=True)
+
+buzzer.frequency = TONE_ON
+buzzer.duty_cycle = 32768
+
+# Rotary Encoder Setup
+encoder = rotaryio.IncrementalEncoder(board.GP7, board.GP6)
+
+switch = digitalio.DigitalInOut(board.GP26)
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.DOWN
+
+last_position = encoder.position
+switch_state = switch.value
+
+# Code
+while True:
